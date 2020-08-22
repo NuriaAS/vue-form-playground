9f48ed7d990818dfa470898c08273d0153582bba
nuriaandresfoto <nuriaandresfoto@gmail.com>
2020-08-22 13:11:12 +0200

    Separete error messages

--- script.js
+++ script.js
@@ -19,10 +19,13 @@ new Vue({
 	el: "#app",
 	data: {
       user: "",
+      userNameHasWrongLength: false,
       mail: "",
+      mailHasNoRequiredCharacter: false,
       password: "",
+      passwordHasWrongString: false, 
+      passwordHasWrongLength: false,
       regions: "espana",
-      message: "",
       passwordErrors: [
          "12345",
          "admin",
@@ -34,26 +37,29 @@ new Vue({
    watch: {
       password(newPass) {
          if(this.passwordErrors.indexOf(newPass) > -1) {
-            this.message = "La contraseña no pude ser: '12345', 'admin' o 'password'";
-            return false;
+            this.passwordHasWrongString = true;
+         } else {
+            this.passwordHasWrongString = false; 
          }
+
          if(newPass.length < 8) {
-            this.message = "La contraseña debe tener el menos 8 caracteres";
-            return false;
+            this.passwordHasWrongLength = true; 
+         } else {
+            this.passwordHasWrongLength = false; 
          }
-         this.message = null;
       },
       mail(newEmail) {
          if(newEmail.includes(this.mailRequiredCharacter)) {
-            this.message = null;
+            this.mailHasNoRequiredCharacter = false;
          } else {
-            this.message = "Introduce un mail con el carácter @";
+            this.mailHasNoRequiredCharacter = true;
          }
       },
       user(userName) {
          if(userName.length > 10) {
-            this.message = "El numbre de usuario no puede superar los 10 caracteres";
-            return false;
+            this.userNameHasWrongLength = true;
+         } else {
+            this.userNameHasWrongLength = false;
          }
       }  
    }
